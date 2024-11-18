c538a32adac5076c12e4023346f5f92e6fed73e9
master367 <adiko7634@gmail.com>
2024-11-19 00:31:21 +0500

    first commit

---
+++ script3page.js
@@ -0,0 +1,16 @@
+function openPage(pageName, elmnt,) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.classList.add("active");
+}
+document.getElementById("defaultOpen").click();
+
+
